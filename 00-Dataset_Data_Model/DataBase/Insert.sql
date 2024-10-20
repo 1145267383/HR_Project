a09f84dfaeb9304d8fc3f7c3be2209c3628e1c17
Mohamed_Abd-Almgyd <mohamedatascience@gmail.com>
2024-10-20 18:11:54 +0300

    update1

--- 00-Dataset_Data_Model/DataBase/Insert.sql
+++
@@ -1,7 +0,0 @@
-BULK INSERT Employees FROM '.\01-Dataset\01-Employee.csv'
-WITH (
-    FIELDTERMINATOR = ',',
-    ROWTERMINATOR = '\n',
-    FIRSTROW = 2
-);
-
---
+++ 00-Dataset_Data_Model/SQL_Create_DB/Insert.sql
@@ -0,0 +1,49 @@
+-- Insert the data from CSV files into the tables.
+
+BULK INSERT Employees FROM '.\01-Dataset\01-Employee.csv'
+WITH (
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2
+);
+
+
+BULK INSERT Employees FROM '.\01-Dataset\01-Employee.csv'
+WITH (
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2
+);
+
+
+BULK INSERT Employees FROM '.\01-Dataset\01-Employee.csv'
+WITH (
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2
+);
+
+
+BULK INSERT Employees FROM '.\01-Dataset\01-Employee.csv'
+WITH (
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2
+);
+
+
+BULK INSERT Employees FROM '.\01-Dataset\01-Employee.csv'
+WITH (
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2
+);
+
+
+BULK INSERT Employees FROM '.\01-Dataset\01-Employee.csv'
+WITH (
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2
+);
+
